084a69062f86f37972561cf71583624337d60d64
Adharsh PB <adharshpb6@gmail.com>
2026-03-25 19:35:24 +0530

    setting

--- backend/settings.py
+++ backend/settings.py
@@ -129,10 +129,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'core.User'
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-]
+CORS_ALLOW_ALL_ORIGINS = True
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
